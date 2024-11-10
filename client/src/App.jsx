6ce8da275615a929f0e076041cf66aae062406e4
Sharanappa <hbdsharanappa52@gmail.com>
2024-11-11 00:03:37 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./App.css"
+import LoginPage from './components/pages/loginpage/LoginPage'
+
+const App = () => {
+  return (
+    <div>
+      <LoginPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/pages/loginpage/LoginPage.jsx
@@ -0,0 +1,144 @@
+import { Box, Button, ButtonGroup, Card, Stack, TextField, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import authImg from "../../../assets/img-auth-big.jpg";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+import styled from '@emotion/styled';
+
+const ButtonsLoginAs = styled(Button)({
+  textTransform: "capitalize"
+
+});
+const LoginPage = () => {
+  const [user, setUser] = useState({
+    username: null,
+    password: null
+  })
+
+
+
+  const handelAsAdmin = () => {
+    setUser({
+      username: "admin",
+      password: "admin@123"
+    })
+
+  }
+  const handelAsEmployee = () => {
+    setUser({
+      username: "sharan",
+      password: "admin@123"
+    })
+
+  }
+  const handelAsClient = () => {
+    setUser({
+      username: "tcs001",
+      password: "admin@123"
+    })
+
+  }
+  return (
+    <div>
+      <Box display="flex" height="100vh" >
+        {/* position="relative" */}
+
+        <Box
+          sx={{
+            backgroundImage: `url(${authImg})`,
+            backgroundSize: "cover",
+            backgroundAttachment: "fixed",
+            backgroundPosition: "center",
+            width: { xs: "100%", md: "70%" },
+            position: "relative",
+            display: { xs: "none", md: "block" }
+          }}
+
+
+        >
+          <Box sx={{
+            color: "white",
+            position: "absolute",
+            bottom: 50,
+            padding: 10
+          }}>
+            <Typography fontSize="1.2rem">
+              <b>People Pro </b>provides you with a powerful and cost-effective HR platform to ensure you get the best from your employees and managers. People Pro is a timely solution to upgrade and modernize your HR team to make it more efficient and consolidate your employee information into one intuitive HR system.
+
+            </Typography>
+
+          </Box>
+        </Box>
+
+        {/* Login Form Container */}
+        <Box
+          sx={{
+            width: "35%",
+            padding: 3,
+            display: 'flex', // Make the box a flex container
+            flexDirection: 'column', // Aligns children vertically
+            justifyContent: 'center', // Center children vertically
+            // alignItems: 'center', // Center children horizontally
+
+          }}
+        >
+          <Typography variant='h5' fontWeight="bold" color='secondary'> People Pro</Typography>
+          <Typography variant='h6'> Welcome to People Pro</Typography>
+          <Typography> Welcome back, Please login into an account</Typography>
+          <Box p={1}>
+            <form>
+              <Stack
+                component="form"
+                spacing={2}
+                noValidate
+                autoComplete="off"
+                sx={{ width: '100%' }} // Ensures the stack stretches to fill its parent
+              >
+                {/* <TextField
+                id="filled-hidden-label-normal"
+                label="username"
+                variant="standard"
+                type='text'
+                fullWidth
+              /> */}
+
+                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                  <AccountCircleIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                  <TextField id="input-with-sx" label="" placeholder='username' fullWidth variant="standard" name='username' value={user.username} />
+                </Box>
+                <Box sx={{ display: 'flex', alignItems: 'flex-end', }}>
+                  <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                  <TextField id="input-with-sx" type='password' label="" placeholder='password' fullWidth variant="standard" name='password' value={user.password} />
+                </Box>
+              </Stack>
+              <Button fullWidth type='submit' sx={{ mt: 2 }} color='primary' variant='contained'>
+                Submit
+              </Button>
+
+
+
+              <br />
+              <br />
+              <ButtonGroup
+                disableElevation
+                variant="outlined"
+                aria-label="Disabled button group"
+                sx={{ ml: 12 }}
+              >
+
+                <ButtonsLoginAs onClick={handelAsAdmin} fullWidth>Admin</ButtonsLoginAs>
+                <ButtonsLoginAs onClick={handelAsEmployee} fullWidth>Employee</ButtonsLoginAs>
+                <ButtonsLoginAs onClick={handelAsClient} fullWidth>Client</ButtonsLoginAs>
+              </ButtonGroup>
+            </form>
+          </Box>
+
+
+        </Box>
+
+      </Box>
+    </div>
+  );
+};
+
+export default LoginPage;
