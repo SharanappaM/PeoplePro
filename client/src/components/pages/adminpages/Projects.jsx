88fce6cb83b5f6dc61a6fe87860106e9f63115bf
Sharanappa <hbdsharanappa52@gmail.com>
2024-11-18 23:38:47 +0530

    commit

--- client/src/components/pages/adminpages/Projects.jsx
+++ client/src/components/pages/adminpages/Projects.jsx
@@ -1,10 +1,178 @@
-import React from 'react'
+import { Box, Button, Card, Divider, FormLabel, MenuItem, Select, TextField, Typography, InputLabel, Modal, Grid2, Grid } from '@mui/material';
+
+import React, { useState } from 'react'
+import FactCheckIcon from '@mui/icons-material/FactCheck';
+import DataTable from 'react-data-table-component';
+import { customStyles } from '../ReactDataTableStyle';
 
 const Projects = () => {
+
+  const [employeesData, setEmployeesData] = useState([]);
+  const [departmentName, setDepartmentName] = useState([]);
+  const [designationName, setDesignationName] = useState([]);
+  const [entries, setEntries] = useState(10);
+  const [searchTerm, setSearchTerm] = useState('');
+
+
+  const columns = [
+    {
+      name: 'First Name',
+      selector: (row) => row.firstname,
+      sortable: true,
+    },
+    {
+      name: 'Last Name',
+      selector: (row) => row.lastname,
+      sortable: true,
+    },
+    {
+      name: 'Contact Number',
+      selector: (row) => row.contactnumber,
+      sortable: true,
+    },
+    {
+      name: 'Gender',
+      selector: (row) => row.gender,
+      sortable: true,
+    },
+    {
+      name: 'Designation',
+      selector: (row) => row.designation,
+      sortable: true,
+    },
+    {
+      name: 'Role',
+      selector: (row) => row.role,
+      sortable: true,
+    },
+
+    {
+      name: 'Status',
+      selector: (row) => "Active",
+      sortable: true,
+    },
+
+    // Add more columns as needed
+  ];
   return (
-    <div>Projects
-      
-    </div>
+    <Box>
+      <Box>
+        <Grid container spacing={4}>
+          <Grid item >
+            <Card sx={{}}>
+              <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box p={2} bgcolor="success.main">
+                  <FactCheckIcon sx={{
+                    fontSize: "60px"
+                  }} />
+                </Box>
+                <Box p={2}>
+                  <Typography variant='h6'>5</Typography>
+                  <Typography variant='h6'>Total Caomplated</Typography>
+                </Box>
+              </Box>
+            </Card>
+          </Grid>
+          <Grid item >
+            <Card sx={{}}>
+              <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box p={2} bgcolor="lightgoldenrodyellow">
+                  <FactCheckIcon sx={{
+                    fontSize: "60px"
+                  }} />
+                </Box>
+                <Box p={2}>
+                  <Typography variant='h6'>2</Typography>
+                  <Typography variant='h6'>Total In Progress</Typography>
+                </Box>
+              </Box>
+            </Card>
+          </Grid>
+          <Grid item >
+            <Card sx={{}}>
+              <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box p={2} bgcolor="lightblue">
+                  <FactCheckIcon sx={{
+                    fontSize: "60px"
+                  }} />
+                </Box>
+                <Box p={2}>
+                  <Typography variant='h6'>5</Typography>
+                  <Typography variant='h6'>Total Not Stated</Typography>
+                </Box>
+              </Box>
+            </Card>
+          </Grid>
+          <Grid item >
+            <Card sx={{}}>
+              <Box display="flex" justifyContent="space-between" alignItems="center">
+                <Box p={2} bgcolor="orangered">
+                  <FactCheckIcon sx={{
+                    fontSize: "60px"
+                  }} />
+                </Box>
+                <Box p={2}>
+                  <Typography variant='h6'>5</Typography>
+                  <Typography variant='h6'>Total On Hold</Typography>
+                </Box>
+              </Box>
+            </Card>
+          </Grid>
+
+        </Grid>
+
+      </Box>
+      <Box mt={4}>
+
+        <Card sx={{ width: "75vw", padding: 2 }}>
+          <Typography variant="h6" mb={2}>List All Projects</Typography>
+          <Divider />
+
+          <Box sx={{ mt: 2, display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography>Show</Typography>
+              <Select
+                value={entries}
+
+                size="small"
+                sx={{ ml: 1 }}
+              >
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={50}>50</MenuItem>
+                <MenuItem value={100}>100</MenuItem>
+              </Select>
+              <Typography sx={{ ml: 1 }}>entries</Typography>
+            </Box>
+
+            <Box sx={{ display: "flex", alignItems: "center" }}>
+              <Typography>Search</Typography>
+              <TextField
+                size="small"
+                sx={{ ml: 1 }}
+                value={searchTerm}
+
+                variant="outlined"
+              />
+            </Box>
+
+            <Button variant='outlined'> Add  </Button>
+          </Box>
+
+          <Box sx={{ mt: 2 }}>
+            <DataTable
+              columns={columns}
+              // data={employeesData.filter(item => item.designation.toLowerCase().includes(searchTerm.toLowerCase()))}
+              data={employeesData}
+              pagination
+              paginationPerPage={entries}
+              customStyles={customStyles}
+            />
+          </Box>
+        </Card>
+
+      </Box>
+
+    </Box>
   )
 }
 
