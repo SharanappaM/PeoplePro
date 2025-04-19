6fc0827966ce75b530b059d183ed1abc91a5c739
Sharanappa <sharanappa@nextiqz.com>
2025-04-20 00:23:30 +0530

    commit

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,58 +1,58 @@
-// local
-import mysql from "mysql2"
+// // local
+// import mysql from "mysql2"
 
-import dotenv from 'dotenv';
-dotenv.config();
-
-const con = mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
-
-})
-
-con.connect(function(error){
-    if(error){
-        return console.log("Connection error");
-        
-    }else{
-        return console.log("Coonected");
-        
-    }
-})
-export default con;
-
-
-
-// // Server 
-// import mysql from "mysql2";
-// import dotenv from "dotenv";
-
+// import dotenv from 'dotenv';
 // dotenv.config();
 
 // const con = mysql.createConnection({
-//   host: process.env.MYSQLHOST,
-//   port: process.env.MYSQLPORT,
-//   user: process.env.MYSQLUSER,
-//   password: process.env.MYSQLPASSWORD,
-//   database: process.env.MYSQLDATABASE,
-// });
+//     host:process.env.DB_HOST,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.DB_DATABASE
 
-// con.connect((error) => {
-//   if (error) {
-//     console.log("❌ Connection error:", error);
-//   } else {
-//     console.log("✅ Connected to the MySQL database!");
-//   }
-// });
+// })
 
-// con.query('SHOW TABLES', (err, results) => {
-//     if (err) {
-//       console.log('❌ Error:', err);
-//     } else {
-//       console.log('✅ Tables:', results);
+// con.connect(function(error){
+//     if(error){
+//         return console.log("Connection error");
+        
+//     }else{
+//         return console.log("Coonected");
+        
 //     }
-//   });
-
+// })
 // export default con;
+
+
+
+// Server 
+import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const con = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
+
+con.connect((error) => {
+  if (error) {
+    console.log("❌ Connection error:", error);
+  } else {
+    console.log("✅ Connected to the MySQL database!");
+  }
+});
+
+con.query('SHOW TABLES', (err, results) => {
+    if (err) {
+      console.log('❌ Error:', err);
+    } else {
+      console.log('✅ Tables:', results);
+    }
+  });
+
+export default con;
